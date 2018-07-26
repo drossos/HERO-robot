d52b96a714995f7cb400eef1c91a5ba292170886
Daniel Rossos <d.rossos.53@gmail.com>
2018-07-26 08:42:19 -0400

    Added BLE support for Tendon glove

--- TendonGlove-2018-05-03-Manual_Auto_Buttons-1/TendonGlove-2018-05-03-Manual_Auto_Buttons-1.ino
+++ TendonGlove-2018-05-03-Manual_Auto_Buttons-1/TendonGlove-2018-05-03-Manual_Auto_Buttons-1.ino
@@ -12,14 +12,10 @@
 #include <Servo.h>
 #include <CurieBLE.h>
 
-BLEService batteryService("180F"); // BLE Battery Service
-BLEService monitorService("4ca04ea0-380a-4aa9-af51-a0d958c28099"); //custom 120-bit UUID for custom Service
+Servo myservoflex;  // create servo object to control a servo
+Servo myservoextend;  // create servo object to control a servo
+
 BLEService motorService("ebbb19a6-c943-44f9-aee0-e180300007f0"); //custom 120-bit UUID custom service
- 
-// BLE Battery Level Characteristic"
-BLEUnsignedCharCharacteristic batteryLevelChar("2A19",  // standard 16-bit characteristic UUID
-    BLERead | BLENotify);     // remote clients will be able to
-// get notifications if this characteristic changes
 
 //motor characteristic
 BLEIntCharacteristic motorReading("00211321-dc03-4f55-82b6-14a630bd8e2d",
@@ -29,11 +25,6 @@ BLERead|BLENotify|BLEWrite);
 BLEIntCharacteristic motorExtendChar("809ba7a9-13ad-4446-a005-bdc12ca93c76", BLERead|BLENotify|BLEWrite);
 BLEIntCharacteristic motorContractChar("2fad8a3f-e1a1-47cd-982b-24e13fbe9342", BLERead|BLENotify|BLEWrite);
 
-int oldBatteryLevel = 0;  // last battery level reading from analog input
-long previousMillis = 0;  // last time the battery level was checked, in ms
-
-Servo myservoflex;  // create servo object to control a servo
-Servo myservoextend;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
 float timeMillis=0;
@@ -49,50 +40,13 @@ int extendButtonOld=0;
 int extendButtonChange=0;
 int delayMode=0;
 int timeMillisDelayMode=0;
-int extendMotor=50;//50 = fully squeeze bottle; 80 = partially squeeze bottle
-int retractMotor=150;//150 = fully extend fingers; 120 = partially extend fingers
-
+int extendMotor=150;
+int retractMotor=50;
 int manualExtend = 0;
 int manualContract = 1;
-int test=0;
+int manualRelax=-1;
 
 void setup() {
-  Serial.begin(9600);    // initialize serial communication
-  pinMode(13, OUTPUT);   // initialize the LED on pin 13 to indicate when a central is connected
-
-  //initalize eventListeners
-  motorReading.setEventHandler(BLEWritten, switchCharacteristicWritten);
-  // begin initialization
-  BLE.begin();
-
-  BLE.setLocalName("LegoHERO");
-  
-  BLE.setAdvertisedService(batteryService);  // add the service UUID
-  batteryService.addCharacteristic(batteryLevelChar); // add the battery level characteristic
-  BLE.addService(batteryService);   // Add the BLE Battery service
-  batteryLevelChar.setValue(oldBatteryLevel);   // initial value for this characteristic
-  
-  BLE.setAdvertisedService(motorService);  // add the service UUID
-  motorService.addCharacteristic(motorReading); 
-  motorService.addCharacteristic(motorExtendChar);
-  motorService.addCharacteristic(motorContractChar);
-  BLE.addService(motorService);   // Add the BLE Battery service
-
-
-  motorReading.setValue(-1);
-  motorExtendChar.setValue(extendMotor);
-  motorContractChar.setValue(retractMotor);
-
-  extendMotor = motorExtendChar.value();
-  retractMotor = motorContractChar.value();
-  
-  /* Start advertising BLE.  It will start continuously transmitting BLE
-     advertising packets and will be visible to remote BLE central devices
-     until it receives a new connection */
-
-  // start advertising
-  BLE.advertise();
-  
   myservoflex.attach(A1);  // attaches the servo
   myservoextend.attach(A0);  // attaches the servo 
   //myservoflex.write(40); 
@@ -101,28 +55,35 @@ void setup() {
   //while (!Serial);    // wait for the serial port to open
   timeMillis=millis();
   timeMillisMotionReset=millis();  
-  pinMode(13, OUTPUT);
   digitalWrite(13,LOW);
-  pinMode(2, OUTPUT);
-  digitalWrite(2,LOW);
-  pinMode(3, INPUT);
-  pinMode(4, OUTPUT);
-  digitalWrite(4,HIGH);
-  pinMode(6, OUTPUT);
-  digitalWrite(6,LOW);
-  pinMode(7, INPUT);
-  pinMode(8, OUTPUT);
-  digitalWrite(8,HIGH);
-  
   CurieIMU.begin();
   // Set the accelerometer range to 2G
   CurieIMU.setAccelerometerRange(2);
   // Set the accelerometer range to 250 degrees/second
   CurieIMU.setGyroRange(250);
 
+  BLE.begin();
+
+  BLE.setLocalName("LegoHERO");
+  
+  BLE.setAdvertisedService(motorService);  // add the service UUID
+  motorService.addCharacteristic(motorReading); 
+  motorService.addCharacteristic(motorExtendChar);
+  motorService.addCharacteristic(motorContractChar);
+  BLE.addService(motorService);   // Add the BLE Battery service
+
+  motorReading.setValue(manualRelax);
+  motorExtendChar.setValue(extendMotor);
+  motorContractChar.setValue(retractMotor);
+
+  extendMotor = motorExtendChar.value();
+  retractMotor = motorContractChar.value();
+
+  BLE.advertise();
+
   //read buttons
-  autoButton=digitalRead(3);
-  extendButton=digitalRead(7);
+  autoButton=digitalRead(A3);
+  extendButton=digitalRead(A2);
 
   // if auto button is pressed down we are in manual mode, start manual mode with motors relaxed
   if (autoButton==1)
@@ -135,11 +96,6 @@ void setup() {
   myservoextend.write(extendMotor); 
   delayMode=1;
   timeMillisDelayMode=millis();}
-
-  myservoflex.write(retractMotor);  
-    myservoextend.write(extendMotor);
-
-    
 }
 
 void loop() {
@@ -173,8 +129,8 @@ void loop() {
   timeMillis=millis();
    
   //read buttons
-  autoButton=digitalRead(3);
-  extendButton=digitalRead(7);
+  autoButton=digitalRead(A3);
+  extendButton=digitalRead(A2);
   
   //reset gyro biases when user is not moving, so that when the user is at rest gtot is 0
   if((gtot<5)&&(timeMillis>timeMillisGyroReset+20000)){
@@ -189,7 +145,6 @@ if ((autoButton==0)&&(delayMode==1)&&(timeMillis<(timeMillisDelayMode+2000)))
 {motionDetect=0;}
 
 else{
-  
   delayMode=0;
   //detect if auto button has been pressed; autoButton=0 is auto mode (button up), autoButton=1 is manual mode (button down)
   if (autoButton!=autoButtonOld){
@@ -243,11 +198,9 @@ else{
     extendButtonChange=1;
   }
 
-  //manual app control will work only if not in auto mode on the robot
-
-  //if extend button is pressed while robot is in manual mode then change characteristic values if extend button is pressed up or flexion if extend button pressed down 
-  else if(((autoButton==1)&&(extendButtonChange==1)&&(extendButton==1))){
-    /*
+  //if extend button is pressed while robot is in manual mode then trigger finger extension if extend button is pressed up or flexion if extend button pressed down 
+  else if((autoButton==1)&&(extendButtonChange==1)&&(extendButton==1)){
+   /*
     myservoflex.write(extendMotor);
     myservoextend.write(retractMotor);
     */
@@ -255,88 +208,36 @@ else{
     extendButtonChange=0;
   }
 
-  else if(((autoButton==1)&&(extendButtonChange==1)&&(extendButton==0))){
-    /*
-    myservoflex.write(retractMotor);  
+  else if((autoButton==1)&&(extendButtonChange==1)&&(extendButton==0)){
+   /*
+    myservoflex.write(retractMotor);
     myservoextend.write(extendMotor);
     */
     motorReading.setValue(manualContract);
     extendButtonChange=0;
+  
   }
 
+  if (autoButton==1){
   //update motors based off of the new characteristic values presented
-  if ( motorReading.value() == manualExtend){
+  if ( motorReading.value() == manualContract){
      myservoflex.write(extendMotor);
     myservoextend.write(retractMotor);
   }
 
-  if ( motorReading.value() == manualContract){
+  if ( motorReading.value() == manualExtend){
     myservoflex.write(retractMotor);  
     myservoextend.write(extendMotor);
+  }
+  
+  if ( motorReading.value() == manualRelax){
+    myservoflex.write(retractMotor);  
+    myservoextend.write(retractMotor);
+  }
   }
   
 }//else for delay mode
-
-//updating battery info
-// listen for BLE peripherals to connect:
-  BLEDevice central = BLE.central();
-
-  // if a central is connected to peripheral:
-  if (central) {
-    //Serial.print("Connected to central: ");
-    // print the central's MAC address:
-   // Serial.println(central.address());
-    // turn on the LED to indicate the connection:
-    digitalWrite(13, HIGH);
-
-    // check the battery level every 200ms
-    // as long as the central is still connected:
-    if (central.connected()) {
-      long currentMillis = millis();
-      // if 200ms have passed, check the battery level:
-      if (currentMillis - previousMillis >= 200) {
-        previousMillis = currentMillis;
-       //M updateBatteryLevel();
-      }
-    }
-    // when the central disconnects, turn off the LED:
-    digitalWrite(13, LOW);
-    //Serial.print("Disconnected from central: ");
-    //Serial.println(central.address());
-  }
-  
 }//end of void loop
-
-void updateBatteryLevel() {
-  /* Read the current voltage level on the A0 analog input pin.
-     This is used here to simulate the charge level of a battery.
-  */
-  int battery = analogRead(A0);
-  int batteryLevel = map(battery, 0, 1023, 0, 100);
-
-  if (batteryLevel != oldBatteryLevel) {      // if the battery level has changed
-    Serial.print("Battery Level % is now: "); // print it
-    Serial.println(batteryLevel);
-    batteryLevelChar.setValue(batteryLevel);  // and update the battery level characteristic
-    oldBatteryLevel = batteryLevel;           // save the level for next comparison
-  }
-}
-
-void switchCharacteristicWritten (BLECentral& central, BLECharacteristic& characteristic){
-  Serial.println("In char callback");
-
-  //update motors based off of the new characteristic values presented
-  if ( motorReading.value() == manualExtend){
-     myservoflex.write(extendMotor);
-    myservoextend.write(retractMotor);
-  }
-
-  if ( motorReading.value() == manualContract){
-    myservoflex.write(retractMotor);  
-    myservoextend.write(extendMotor);
-  }
-  
-}
 
 /*
    Copyright (c) 2016 Intel Corporation.  All rights reserved.
